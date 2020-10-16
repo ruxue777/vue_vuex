c53c33468ce2c0b7d83ac7e4ab08252230dbbab5
Ruxue <1078134231@qq.com>
2020-10-16 22:21:36 +0800

    state状态设置

--- src/store/index.js
+++ src/store/index.js
@@ -6,12 +6,18 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   //状态值
   state: {
-    count:0
+    count:0,
+    name:'ruxue',
+    age:18,
+    sex:'man'
   },
   //修改状态
   mutations: {
     increment(state){
       state.count++
+    },
+    setAge(state,value){
+      state.age = value
     }
   },
   actions: {
---
+++ src/views/Preson.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="preson">
+      <ul>
+          <li>name:{{name}}</li>
+          <li>sex:{{sex}}</li>
+          <li>age:{{age}}</li>
+      </ul>
+      <input type="text" @input="setAge" :value="age"> {{test}}
+  </div>
+</template>
+
+<script>
+import {mapState} from 'vuex';
+export default {
+    //数组方法
+    //computed : mapState(['name','sex','age']),
+
+    //对象方法
+    computed : {
+        test(){
+            return 'test'
+        },
+        ...mapState({
+            name: state => state.name,
+            sex : 'sex',
+            age(state){
+                return state.age
+            }
+        })
+    },
+
+
+    methods:{
+        setAge(e){
+            this.$store.commit('setAge',e.target.value)
+        }
+    }
+    // computed:{
+    //     name(){
+    //         return this.$store.state.name
+    //     },
+    //     sex(){
+    //         return this.$store.state.sex
+    //     },
+    //     age:{
+    //         get(){
+    //             return this.$store.state.age
+    //         },
+    //         set(value){
+    //             this.$store.commit('setAge',value)
+    //         }
+    //     }
+    // }
+}
+</script>
+
+<style>
+
+</style>
